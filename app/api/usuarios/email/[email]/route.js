a93cf38947ef7dde095ead9d33d3d2aa3d3ea11d
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2025-07-10 13:47:26 -0600

    refactor: Update email sending function to internal scope
    - Changed the sendEmail function to be an internal helper, no longer exported.
    - Maintained the existing email sending logic while ensuring the environment variable check for the Resend API key remains intact.

--- app/api/usuarios/email/[email]/route.js
+++ app/api/usuarios/email/[email]/route.js
@@ -8,13 +8,13 @@ import { Resend } from "resend";
 if (!process.env.RESEND_API_KEY) {
   throw new Error("RESEND_API_KEY is not set in environment variables");
 }
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail({ to, subject, html }) {
+// Función auxiliar interna, no exportada
+async function sendEmail({ to, subject, html }) {
   try {
     const { data, error } = await resend.emails.send({
-      from: "info@psicologopuebla.com", // Remitente actualizado
+      from: "info@psicologopuebla.com",
       to,
       subject,
       html,
